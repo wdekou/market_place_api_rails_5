50681f82d7a82370fc7b0f34cd10a088a9a595f0
williamdekou <williamdekou@gmail.com>
2017-02-27 20:26:16 +0100

    Adds user firsts specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,8 @@ describe User do
   it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
 end
