ce30964a668dc44a541aecce64b32ae093fda106
williamdekou <williamdekou@gmail.com>
2017-02-27 21:13:23 +0100

    Adds show action the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   #Api definition
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-    # We are going to list our resources here
+      # We are going to list our resources here
+      resource :users, :only => [:show]
     end
   end
 
