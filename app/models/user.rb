7c2226506277ec607576a17bef0eb2f7cc5bc916
williamdekou <williamdekou@gmail.com>
2017-03-01 04:18:47 +0100

    Adds user authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  before_create :generate_authentication_token!
+
+  validates :auth_token, uniqueness: true
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
